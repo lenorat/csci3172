4d13f30a72c24982eb39b707b5a7762d4324a8fe
Lenora Tairova <ln242427@dal.ca>
2024-01-31 15:45:14 -0400

    changes

--- labs/lab3/script.js
+++ labs/lab3/script.js
@@ -1,11 +1,12 @@
-function displayFormData(){
+function displayFormData() {
     var form = document.getElementById("labForm");
     var data = new FormData(form);
     var show = document.getElementById("displayData");
     var display = "<h2>Submitted Information: </h2>";
 
-    for(var input of data.entries()){
-        display +="<p>" + input[0] +": " + pair[1] + "</p>";
+    for (var input of data.entries()) {
+        display += "<p>" + input[0] + ": " + input[1] + "</p>";
     }
+
     show.innerHTML = display;
 }
