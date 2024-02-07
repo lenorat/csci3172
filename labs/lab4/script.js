764a83cb603e6b17ffd75b6c5373374f4fae54c7
Lenora Tairova <ln242427@dal.ca>
2024-02-07 15:35:55 -0400

    lab 4 part 2

--- labs/lab4/script.js
+++ labs/lab4/script.js
@@ -31,3 +31,49 @@ function changeBackgroundColor() {
             container.style.backgroundColor = 'lightpink';
     }
 }
+
+function processForm(event) {
+    event.preventDefault();
+    
+    var number = parseInt(document.getElementById('numberIn').value);
+    var result = '';
+
+    //Check if number is odd or even
+    if (number % 2 === 0) {
+        result += 'The number entered is an even ';
+    } else {
+        result += 'The number entered is an odd ';
+    }
+
+    //Check if number is prime or composite
+    var isPrime = true;
+    if (number === 1) {
+        isPrime = false;
+    } else if (number > 1) {
+        for (var i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    if (isPrime) {
+        result += ' prime number ';
+    } else {
+        result += ' composite number ';
+    }
+
+    // Check the range of the input number
+    if (number <= 50) {
+        result += 'less than or equal to 50.';
+    } else if (number <= 75) {
+        result += 'greater than 50 and less than or equal to 75.';
+    } else if (number <= 100) {
+        result += 'greater than 75 and less than or equal to 100.';
+    } else {
+        result += 'greater than 100.';
+    }
+
+    // Display the result message
+    document.getElementById('displayResult').innerText = result;
+}
