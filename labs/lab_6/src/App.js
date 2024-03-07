b5486ba7b76a088128f3b67f59a9257b806e1d48
lenorat <lenora.tairova@gmail.com>
2024-03-07 11:56:31 -0400

    edited app.js

--- labs/lab_6/src/App.js
+++ labs/lab_6/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lab 6 CSCI3172
         </a>
       </header>
     </div>
